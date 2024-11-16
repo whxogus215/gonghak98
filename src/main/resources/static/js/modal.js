2b8b0a1bada9f8cc1aee93c1f71d9d7867049d65
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2024-11-16 14:15:30 +0900

    feat: 모달창 클릭 시 열기/닫기 트리거 연결

---
+++ src/main/resources/static/js/modal.js
@@ -0,0 +1,9 @@
+function openModal() {
+    document.getElementById("modal-background").style.display = "block";
+    document.getElementById("modal-container").style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById("modal-background").style.display = "none";
+    document.getElementById("modal-container").style.display = "none";
+}
