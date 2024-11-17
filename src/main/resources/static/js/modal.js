94308d5a36f677ba499b48f8001743d4ad2c5735
Taehyeon Jo <70999462+whxogus215@users.noreply.github.com>
2024-11-17 16:41:27 +0900

    feat: 특정 타입의 상세보기를 눌렀을 때, 해당 타입의 테이블만 표시되도록 구현

--- src/main/resources/static/js/modal.js
+++ src/main/resources/static/js/modal.js
@@ -1,4 +1,17 @@
-function openModal() {
+function openModal(typeName) {
+
+    const allTbody = document.querySelectorAll(".modal-body tbody");
+    allTbody.forEach(tbody => {
+        tbody.style.display = "none";
+    });
+    
+    const findTbody = document.getElementById(typeName);
+    if (findTbody) {
+        findTbody.style.display = "table-row-group";
+    } else {
+        console.error(`${typeName}에 해당하는 상세정보가 존재하지 않습니다.`);
+    }
+
     document.getElementById("modal-background").style.display = "block";
     document.getElementById("modal-container").style.display = "block";
 }
